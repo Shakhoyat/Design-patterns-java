fc9a62a1d13b468651ee358f8b636fee3b308148
Shakhoyat <shakoyatsujon@gmail.com>
2025-09-21 00:57:04 +0600

    ok

--- 2.factory/Factory.java
+++ 2.factory/Factory.java
@@ -34,7 +34,6 @@ class FactoryDemo {
     public static void main(String[] args) {
         Animal dog = AnimalFactory.createAnimal("dog");
         Animal cat = AnimalFactory.createAnimal("cat");
-
         dog.makeSound(); // Woof!
         cat.makeSound(); // Meow!
     }
